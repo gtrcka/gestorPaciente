31ae83f4f8977fad1de74836278a5cd20f5aca86
gabi_ <gabi_@192.168.0.80>
2022-04-07 11:16:34 -0300

    ajustes en el código

--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -22,6 +22,18 @@ public class Conexion {
         }
         return conexion;
     }
+    
+    public Connection getcon() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String unicode = "useSSL=false&autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8";
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/gestorpaciente?" + unicode, "root", "1234");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("couldn't connect!");
+            throw new RuntimeException(ex);
+        }
+    }
 }
 
 
